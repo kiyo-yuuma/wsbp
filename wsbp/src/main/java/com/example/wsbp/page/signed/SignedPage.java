7bd29bb2a89782090943d81467d5a9354e8d2591
kiyo-yuuma <kiyokawa0702@outlook.jp>
2023-11-21 23:46:04 +0900

    認証機能追加

--- wsbp/src/main/java/com/example/wsbp/page/signed/SignedPage.java
+++ wsbp/src/main/java/com/example/wsbp/page/signed/SignedPage.java
@@ -1,10 +1,15 @@
 package com.example.wsbp.page.signed;
 
+import org.apache.wicket.authroles.authorization.strategies.role.Roles;
+import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.Model;
 import org.wicketstuff.annotation.mount.MountPath;
 
+// ↓ どの役割のユーザであれば表示してよいか。
+// セッションのgetRoleメソッドが USER であれば表示し、それ以外は表示しない。
+@AuthorizeInstantiation(Roles.USER)
 @MountPath("Signed")
 public class SignedPage extends WebPage {
     public SignedPage() {
