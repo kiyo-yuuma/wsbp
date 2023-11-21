1f0c77ca5ec24d14503df9a6edc9914a5082fdfa
kiyo-yuuma <kiyokawa0702@outlook.jp>
2023-11-21 23:50:29 +0900

    認証停止機能追加

--- wsbp/src/main/java/com/example/wsbp/page/signed/SignedPage.java
+++ wsbp/src/main/java/com/example/wsbp/page/signed/SignedPage.java
@@ -1,9 +1,12 @@
 package com.example.wsbp.page.signed;
 
+import com.example.wsbp.MySession;
+import com.example.wsbp.page.SignPage;
 import org.apache.wicket.authroles.authorization.strategies.role.Roles;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.Model;
 import org.wicketstuff.annotation.mount.MountPath;
 
@@ -16,5 +19,17 @@ public class SignedPage extends WebPage {
         var name = Model.of("test");
         var userNameLabel = new Label("userName", name);
         add(userNameLabel);
+
+        Link<Void> signoutLink = new Link<Void>("signout") {
+
+            @Override
+            public void onClick() {
+                // セッションの破棄
+                MySession.get().invalidate();
+                // SignPageへ移動
+                setResponsePage(SignPage.class);
+            }
+        };
+        add(signoutLink);
     }
 }
